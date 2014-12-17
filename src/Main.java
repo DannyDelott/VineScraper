47e4d7902355c78a647a869e2620ddd19f2e0303
dannydelott <dannydelott@gmail.com>
2014-12-17 15:52:23 -0800

    Modify Main.java
    - Create two TweetBuffer objects to switch back and forth between collecting and processing Status objects.
    - Create urls HashSet to store duplicate Vine urls.
    - Create a StatusListener to fill the buffers and send them to processing threads when rate-limited.
    - Invoke .filter() to begin listening to the Twitter Streaming APIs.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+import java.util.HashSet;
+
 import twitter4j.FilterQuery;
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
 import twitter4j.TwitterStream;
 
 public class Main {
 
 	private static TwitterStream twitter;
+
+	private static TweetBuffer buffer1;
+	private static TweetBuffer buffer2;
+	private static int currentBuffer;
+	private static HashSet<String> urls;
 
 	public static void main(String[] args) {
 
@@ -14,6 +25,56 @@ public class Main {
 		FilterQuery fq = new FilterQuery();
 		String keyword[] = { "http" };
 		fq.track(keyword);
-	}
 
+		// instantiates buffers
+		buffer1 = new TweetBuffer();
+		buffer2 = new TweetBuffer();
+		currentBuffer = 1;
+		urls = new HashSet<String>();
+
+		// instantiates listener to fill and process the buffers
+		StatusListener listener = new StatusListener() {
+
+			@Override
+			public void onStatus(Status status) {
+
+				if ((currentBuffer == 1) && !buffer1.isProcessing()) {
+					buffer1.addStatus(status);
+					return;
+				}
+
+				if ((currentBuffer == 2) && !buffer2.isProcessing()) {
+					buffer2.addStatus(status);
+					return;
+				}
+			}
+
+			@Override
+			public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+				processCurrentBuffer(); // see Step 4
+			}
+
+			@Override
+			public void onDeletionNotice(StatusDeletionNotice deletionNotice) {
+			}
+
+			@Override
+			public void onException(Exception ex) {
+				ex.printStackTrace();
+			}
+
+			@Override
+			public void onScrubGeo(long arg0, long arg1) {
+			}
+
+			@Override
+			public void onStallWarning(StallWarning arg0) {
+				System.out.println(arg0.getMessage());
+			}
+		};
+
+		// starts scraping Vine videos
+		twitter.addListener(listener);
+		twitter.filter(fq);
+	}
 }
