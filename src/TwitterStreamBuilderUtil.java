926a55eb49b2adaaaf3590f5036399f68b0da77a
dannydelott <dannydelott@gmail.com>
2014-12-17 15:07:11 -0800

    Add TwitterStreamBuilderUtil
    - Add Apache Commons IO and Twitter4J libraries to build path.
    - Create TwitterStreamBuilderUtil to manage OAuth connection to Twitter Streaming APIs.

---
+++ src/TwitterStreamBuilderUtil.java
@@ -0,0 +1,20 @@
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class TwitterStreamBuilderUtil {
+
+	public static TwitterStream getStream() {
+
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true);
+		cb.setOAuthConsumerKey("*************************");
+		cb.setOAuthConsumerSecret("**************************************************");
+		cb.setOAuthAccessToken("**************************************************");
+		cb.setOAuthAccessTokenSecret("*********************************************");
+
+		return new TwitterStreamFactory(cb.build()).getInstance();
+
+	}
+
+}
