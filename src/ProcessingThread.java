8f41a1f36bf90a28ca1ef922a0d031b31a779922
dannydelott <dannydelott@gmail.com>
2014-12-18 13:06:57 -0800

    Modify ProcessingThread.java, Add VineUtil.java
    - Move all Vine parsing methods from ProcessingThread.java into VineUtil.java.

--- src/ProcessingThread.java
+++ src/ProcessingThread.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashSet;
@@ -10,7 +7,6 @@ import java.util.HashSet;
 import org.apache.commons.io.FileUtils;
 
 import twitter4j.Status;
-import twitter4j.URLEntity;
 
 public class ProcessingThread implements Runnable {
 
@@ -47,7 +43,7 @@ public class ProcessingThread implements Runnable {
 		for (Status status : tweets) {
 
 			// gets the Vine URL (eg: https://vine.co/v/OW0ei1Uauxv)
-			url = getVineUrl(status);
+			url = VineUtil.getVineUrl(status);
 			if (url == null) {
 				continue;
 			}
@@ -63,7 +59,7 @@ public class ProcessingThread implements Runnable {
 
 			// gets HTML from Vine URL
 			try {
-				html = sendGet(url);
+				html = VineUtil.sendGet(url);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -72,7 +68,7 @@ public class ProcessingThread implements Runnable {
 			}
 
 			// parses out the download URL
-			downloadUrl = parseDownloadUrl(html);
+			downloadUrl = VineUtil.parseDownloadUrl(html);
 			if (downloadUrl == null) {
 				continue;
 			}
@@ -95,82 +91,6 @@ public class ProcessingThread implements Runnable {
 
 		// invokes finished listener
 		listener.onProcessFinished(buffer, urls, numVinesScraped);
-	}
-
-	// private methods
-	private String getVineUrl(Status status) {
-
-		String vineUrl;
-
-		for (URLEntity url : status.getURLEntities()) {
-			if (url.getExpandedURL().contains("vine.co/v/")) {
-				vineUrl = url.getExpandedURL();
-				return vineUrl;
-			}
-		}
-
-		return null;
-	}
-
-	private static String parseDownloadUrl(String html) {
-
-		// if 404 or the url suffix is missing from the html
-		if (html == null || html.contains("Page not found")
-				|| html.contains("<title>Vine</title>")) {
-			return null;
-		}
-
-		// parses out the .mp4 file path
-		try {
-			String[] split = html.split("\"twitter:player:stream\" content=\"");
-			split = split[1].split("\\?versionId");
-			return split[0];
-
-		} catch (ArrayIndexOutOfBoundsException e1) {
-			e1.printStackTrace();
-		}
-
-		return null;
-	}
-
-	private static String sendGet(String u) throws IOException {
-		String url = u;
-		String USER_AGENT = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36";
-		URL obj = new URL(url);
-
-		try {
-			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-			// handles the response code 301 redirect from vine URLs
-			String redirect = con.getHeaderField("Location");
-			if (redirect != null) {
-				obj = new URL(redirect);
-				con = (HttpURLConnection) obj.openConnection();
-			}
-
-			con.setRequestMethod("GET");
-			con.setRequestProperty("User-Agent", USER_AGENT);
-
-			int responseCode = con.getResponseCode();
-			if (responseCode > 200) {
-				return null;
-			}
-
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-
-			return response.toString();
-
-		} catch (Exception e) {
-			return null;
-		}
 	}
 
 	// getters
---
+++ src/VineUtil.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import twitter4j.Status;
+import twitter4j.URLEntity;
+
+public class VineUtil {
+
+	public static String getVineUrl(Status status) {
+
+		String vineUrl;
+
+		for (URLEntity url : status.getURLEntities()) {
+			if (url.getExpandedURL().contains("vine.co/v/")) {
+				vineUrl = url.getExpandedURL();
+				return vineUrl;
+			}
+		}
+
+		return null;
+	}
+
+	public static String parseDownloadUrl(String html) {
+
+		// if 404 or the url suffix is missing from the html
+		if (html == null || html.contains("Page not found")
+				|| html.contains("<title>Vine</title>")) {
+			return null;
+		}
+
+		// parses out the .mp4 file path
+		try {
+			String[] split = html.split("\"twitter:player:stream\" content=\"");
+			split = split[1].split("\\?versionId");
+			return split[0];
+
+		} catch (ArrayIndexOutOfBoundsException e1) {
+			e1.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public static String sendGet(String u) throws IOException {
+		String url = u;
+		String USER_AGENT = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36";
+		URL obj = new URL(url);
+
+		try {
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+			// handles the response code 301 redirect from vine URLs
+			String redirect = con.getHeaderField("Location");
+			if (redirect != null) {
+				obj = new URL(redirect);
+				con = (HttpURLConnection) obj.openConnection();
+			}
+
+			con.setRequestMethod("GET");
+			con.setRequestProperty("User-Agent", USER_AGENT);
+
+			int responseCode = con.getResponseCode();
+			if (responseCode > 200) {
+				return null;
+			}
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			return response.toString();
+
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}
